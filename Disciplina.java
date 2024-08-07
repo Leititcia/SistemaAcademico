3f51d283ca2c7e6c12415cda501105931f20432c
Leititcia <pereiraleticia337@gmail.com>
2024-08-06 22:20:17 -0300

    :sparkles: feat: cria esqueleto das funções

---
+++ Disciplina.java
@@ -0,0 +1,29 @@
+package rAcademico;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Disciplina {
+
+	int idDisciplina;
+	String nomeDisciplina;
+	int cargaHoraria;
+	int creditos;
+	
+	Disciplina() {
+	
+	}
+	
+	Disciplina(int idDisciplina, String nomeDisciplina, int cargaHoraria, int creditos) {
+		 this.idDisciplina = idDisciplina;
+		 this.nomeDisciplina = nomeDisciplina;
+		 this.cargaHoraria = cargaHoraria;
+		 this.creditos = creditos;
+	}
+		    
+
+
+	
+}
