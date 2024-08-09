1203c791727a455d024db8b4ae60571215aef55e
Letícia Pereira do Vale <130941056+Leititcia@users.noreply.github.com>
2024-08-09 18:12:24 -0300

    :recycle: refactor: atualiza código e caminho do arquivo

---
+++ src/Turma.java
@@ -0,0 +1,25 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Turma {
+
+    public int nomeTurma;
+    public Disciplina disciplina;
+
+    Turma() {
+
+    }
+
+    Turma(int nomeTurma) {
+        this.nomeTurma = nomeTurma;
+        this.disciplina = disciplina;
+    }
+
+
+    public static void listarTurmas() {
+
+    }
+
+}
