b578086da1abde953e479301ff248c26975160c6
Letícia Pereira do Vale <130941056+Leititcia@users.noreply.github.com>
2024-08-09 18:07:05 -0300

    :recycle: refactor: atualiza pastas, caminhos de arquivo e funções

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+package Main;
+import java.util.Scanner; // programa utiliza a classe Scanner
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // cria um Scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("*******************************");
+        System.out.println("Bem-vindo ao Sistema Acadêmico!");
+        System.out.println("*******************************");
+
+        System.out.println("\nSelecione uma opção:");
+        System.out.println("1. Cadastrar disciplina");
+        System.out.println("2. Cadastrar professor e associar à disciplina");
+        System.out.println("3. Listar turmas por disciplina");
+
+        int op = input.nextInt();
+
+        if (op == 1) {
+
+            System.out.println("\n---------------------------------");
+            Disciplina.cadastrarDisciplina();
+            System.out.println("---------------------------------\n");
+
+        } else if (op == 2) {
+		
+            System.out.println("\n---------------------------------");
+            Professor.cadastrarProfessor();
+            System.out.println("\n---------------------------------");
+		
+        } else if (op == 3) {
+		
+            System.out.println("\n---------------------------------");
+            Turma.listarTurmas();
+            System.out.println("\n---------------------------------");
+		
+        } else {
+		
+            System.out.println("Escolha uma opção válida.");
+        }
+
+
+    }
+
+}
