c58d845e086f6b9048b2e5eace18af9464429a7d
Letícia Pereira do Vale <130941056+Leititcia@users.noreply.github.com>
2024-08-09 20:36:52 -0300

    :recycle: refactor: update Turma.java

--- src/Turma.java
+++ src/Turma.java
@@ -1,25 +1,40 @@
-package Main;
-
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Turma {
+    public String nomeTurma;
 
-    public int nomeTurma;
-    public Disciplina disciplina;
-
-    Turma() {
+    public Turma() {
 
     }
-
-    Turma(int nomeTurma) {
+    public Turma(String nomeTurma) {
         this.nomeTurma = nomeTurma;
-        this.disciplina = disciplina;
     }
 
+    public Turma cadastraTurma() {
+        Scanner input = new Scanner(System.in);
 
-    public static void listarTurmas() {
+        System.out.println("Nome dessa turma suja e fedida: ");
+        String nomeTurma = input.nextLine();
 
+        Turma turma = new Turma(nomeTurma);
+
+        return turma;
+    }
+
+    public static void listarTurmas(List<Disciplina> disciplinas) {
+        if (!disciplinas.isEmpty()) {
+            System.out.println("Turmas para a disciplina :");
+             for (Disciplina disciplina : disciplinas) {
+                for (Turma turma : disciplina.professor.turmas) {
+                    System.out.println("Turma: " + turma.nomeTurma +
+                            ", Professor: " + disciplina.professor.nome);
+                }
+            }
+        }else {
+
+            System.out.println("Não há turmas cadastradas");
+        }
     }
 
 }
