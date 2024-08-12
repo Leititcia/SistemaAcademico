c5154c6a5c1d3fca00cfaa6a01e6bce9629d04eb
Letícia Pereira do Vale <130941056+Leititcia@users.noreply.github.com>
2024-08-12 00:20:40 -0300

    :sparkles: feat: atualiza funcionalidade

--- src/Professor.java
+++ src/Professor.java
@@ -1,20 +1,15 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class Professor {
-    public int idProfessor;
-    public String nome;
+    private int idProfessor;
+    private String nome;
 
-    public static List<Turma> turmas;
-
-    public Professor() {}
-
-    public Professor(int idProfessor, String nome, List<Turma> turmas) {
+    public Professor(int idProfessor, String nome) {
         this.idProfessor = idProfessor;
         this.nome = nome;
-        this.turmas = turmas;
     }
 
+    // Método para cadastrar um novo professor
     public static Professor cadastrarProfessor() {
         Scanner input = new Scanner(System.in);
 
@@ -25,14 +20,15 @@ public class Professor {
         System.out.println("Informe o nome do professor:");
         String nome = input.nextLine();
 
-        System.out.println("Professor cadastrado!");
+        return new Professor(idProfessor, nome);
+    }
 
-        Turma turma = new Turma();
-        Turma turma1 = turma.cadastraTurma();
-        turmas.add(turma1);
-        Professor professor = new Professor(idProfessor, nome, turmas);
+    // Getters
+    public int getIdProfessor() {
+        return idProfessor;
+    }
 
-        return professor;
-
+    public String getNome() {
+        return nome;
     }
 }
